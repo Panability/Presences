c14a27bc6d25d558e4e4246cf2626041fb53eb29
Nam Anh <phamnamanh25@gmail.com>
2021-12-25 19:35:48 +0700

    Remove unused variables, remove deprecated function & format code

--- websites/A/Audible/presence.ts
+++ websites/A/Audible/presence.ts
@@ -2,26 +2,17 @@ const presence = new Presence({
     clientId: "924198999806857236"
   }),
   browsingStamp = Math.floor(Date.now() / 1000);
-let user: HTMLElement,
-  title: HTMLElement,
-  replace: HTMLElement,
-  search: HTMLElement;
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-    largeImageKey: "audiblelogo"
+    largeImageKey: "audiblelogo",
+    startTimestamp: browsingStamp
   };
-  presenceData.startTimestamp = browsingStamp;
-
   if (document.location.pathname === "/audible.com")
     presenceData.details = "Viewing Audible homepage";
   else if (document.querySelector(".firstHeading") !== null) {
     presenceData.details = "Viewing page:";
     presenceData.state = document.querySelector(".firstHeading").textContent;
   }
-
-  if (!presenceData.details) {
-    presence.setTrayTitle();
-    presence.setActivity();
-  } else presence.setActivity(presenceData);
+  presence.setActivity(presenceData);
 });
